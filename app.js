43a519c3bf9c232f2d3d3078f6ba54a532d1a3f3
KenVandemar <duykhanh1310@gmail.com>
2016-05-09 11:34:42 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://ken:khanhninh@ds017852.mlab.com:17852/yelpcamp");
+console.log(process.env.DATABASEURL);
+
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://ken:khanhninh@ds017852.mlab.com:17852/yelpcamp");
+
  
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -51,4 +54,4 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
-})
+});
